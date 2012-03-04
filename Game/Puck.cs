153359bb1a14de118277c6a84432ba8c0ed3af62
Tim G. Thomas <tim@timgthomas.com>
2012-03-04 10:48:50 -0600

    puck animation

--- Game/Puck.cs
+++ Game/Puck.cs
@@ -9,25 +9,32 @@ namespace Ping.Game
 
 		private Texture2D _texture;
 
+		private Vector2 _position;
+		private Vector2 _velocity;
+
 		public Puck(Ping game) : base(game)
 		{
 			_ping = game;
+
+			_position = new Vector2(10.0f, 10.0f);
+			_velocity = new Vector2(1.0f, 1.0f);
 		}
 
 		protected override void LoadContent()
 		{
 			_texture = Game.Content.Load<Texture2D>("puck");
-
-			base.LoadContent();
 		}
 
 		public override void Draw(GameTime gameTime)
 		{
 			_ping.SpriteBatch.Begin();
-
-			_ping.SpriteBatch.Draw(_texture, Vector2.Zero, Color.White);
-
+			_ping.SpriteBatch.Draw(_texture, _position, Color.White);
 			_ping.SpriteBatch.End();
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			_position = _position + _velocity;
 		}
 	}
 }
